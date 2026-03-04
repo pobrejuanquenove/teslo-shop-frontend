966b464fac7dc0760b1d10c4264f853a0d50499c
Javier Gimenez <jsgimenez@gmail.com>
2026-03-04 16:51:47 -0300

    removed unused imports

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
 import { ProductPage } from "./shop/pages/product/ProductPage";
